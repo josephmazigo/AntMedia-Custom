5057a1260b91d19e323d33d820e3f56bb3497755
Akmall <akmalfaseh18@gmail.com>
2022-08-02 13:25:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+![image](https://user-images.githubusercontent.com/54481799/95862105-16cb0e00-0d6b-11eb-9087-88888889825d.png)
+
+## Basic overview
+
+Ant Media Server is a streaming engine software that provides adaptive, ultra low latency streaming by using 
+WebRTC technology with ~0.5 seconds latency or low latency by using HLS or CMAF. Ant Media Server is highly scalable, 
+running on-premises or on any cloud provider of your choice.
+
+## About Flutter SDK
+
+This repository includes Ant Media Flutter SDK for WebRTC. 
+
+## Integration 
+
+In order to integrate Flutter SDK to your project, please follow [this link](https://resources.antmedia.io/docs/flutter-sdk).
+
+## Support
+
+Have any questions about the Flutter SDK? Visit our [community platform](http://community.antmedia.io/).
+
+## Issues
+Create issues on the [Ant-Media-Server](https://github.com/ant-media/Ant-Media-Server/issues)
---
+++ lib/src/utils/websocket.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+typedef OnMessageCallback = void Function(dynamic msg);
+typedef OnCloseCallback = void Function(int code, String reason);
+typedef OnOpenCallback = void Function();
+
+class SimpleWebSocket {
+  String _url;
+  var _socket;
+  OnOpenCallback? onOpen;
+  OnMessageCallback? onMessage;
+  OnCloseCallback? onClose;
+
+  SimpleWebSocket(this._url);
+
+
+  connect() async {
+    try {
+      _socket = await WebSocket.connect(_url);
+      //_socket = await _connectForSelfSignedCert(_url);
+
+      if (this.onOpen != null) {
+        this.onOpen!();
+      }
+
+      _socket.listen((data) {
+        if (this.onMessage != null) {
+          this.onMessage!(data);
+        }
+      }, onDone: () {
+        if (this.onClose != null) {
+          this.onClose!(_socket.closeCode, _socket.closeReason);
+        }
+      });
+    } catch (e) {
+      if (this.onClose != null) {
+        this.onClose!(500, e.toString());
+      }
+    }
+  }
+
+ void send(data) {
+    if (_socket != null) {
+      _socket.add(data);
+      print('send: $data');
+    }
+  }
+
+  close() {
+    if (_socket != null) _socket.close();
+  }
+
+}
